cf8863deba780941f4b85427cf5d137e93efc5bc
Евгений Квач <Evgeniy.Kvach@newdv.ru>
2019-06-26 08:17:24 +0300

    controller, http

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Studying angularJS.
-w
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,33 @@
-const phonecatApp = angular.module("phonecatApp", [])
-    .controller('DemoCtrl', function($scope) {
-      $scope.name = "World";
-    });
+const phonecatApp = angular.module("phonecatApp", []);
+
+const PhoneListCtrl = ($scope, $http) => {
+  $scope.title = 'Телефоны';
+  const obj = {
+    'url': 'phones/phones.json'
+  }
+  $http(obj).then(response => $scope.phones = response.data);
+
+  $scope.today = new Date;
+  $scope.doneAndFilter = (phoneItem) => phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true ;
+  $scope.sortField = undefined;
+  $scope.reverse = false;
+  
+  $scope.sort = (fieldName) => {
+    if ($scope.sortField === fieldName) {
+      $scope.reverse = !$scope.reverse;
+    }
+    else {
+      $scope.sortField = fieldName;
+      $scope.reverse = false;
+    }
+  };
+
+  $scope.isSortUp = (fieldName) => {
+    return $scope.sortField === fieldName && !$scope.reverse;
+  };
+  $scope.isSortDown = function(fieldName) {
+    return $scope.sortField === fieldName && $scope.reverse;
+  }
+};
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', PhoneListCtrl]); // можно передать анонимную функцию внутри массива
+
