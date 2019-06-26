d7245684391acba5da8561835278a999139134d6
Евгений Квач <Evgeniy.Kvach@newdv.ru>
2019-06-26 09:29:57 +0300

    next step

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,33 +1,18 @@
-const phonecatApp = angular.module("phonecatApp", []);
+'use strict';
 
-const PhoneListCtrl = ($scope, $http) => {
-  $scope.title = 'Телефоны';
-  const obj = {
-    'url': 'phones/phones.json'
-  }
-  $http(obj).then(response => $scope.phones = response.data);
+/* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
 
-  $scope.today = new Date;
-  $scope.doneAndFilter = (phoneItem) => phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true ;
-  $scope.sortField = undefined;
-  $scope.reverse = false;
-  
-  $scope.sort = (fieldName) => {
-    if ($scope.sortField === fieldName) {
-      $scope.reverse = !$scope.reverse;
-    }
-    else {
-      $scope.sortField = fieldName;
-      $scope.reverse = false;
-    }
-  };
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http) {
+    $scope.title = 'Телефоны';
+    const obj = {
+      "method": "GET",
+      "url":'phones/phones.json'
+    };
+    $http(obj).then(response => {
+        $scope.phones = response.data;
+    });
+}]);
 
-  $scope.isSortUp = (fieldName) => {
-    return $scope.sortField === fieldName && !$scope.reverse;
-  };
-  $scope.isSortDown = function(fieldName) {
-    return $scope.sortField === fieldName && $scope.reverse;
-  }
-};
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', PhoneListCtrl]); // можно передать анонимную функцию внутри массива
+
 
