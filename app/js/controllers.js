d6c3dbd3781953844516e5211dc9ed28056efac9
Евгений Квач <Evgeniy.Kvach@newdv.ru>
2019-06-27 09:27:55 +0300

    location&route

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,48 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute']);
 
-phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http) {
+phonecatApp.config(['$routeProvider', '$locationProvider', function($routeProvide,$locationProvider) {
+  $locationProvider
+  .html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+  $routeProvide
+  .when('/',{
+    templateUrl:'template/home.html',
+    controller:'PhoneListCtrl'
+  })
+  .when('/about',{
+    templateUrl:'template/about.html',
+    controller:'AboutCtrl'
+  })
+  .when('/contact',{
+    templateUrl:'template/contact.html',
+    controller:'ContactCtrl'
+  })
+  .when('/phones/:phoneId', {
+    templateUrl: 'template/phone-detail.html',
+    controller: 'PhoneDetailCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
+}]);
+
+phonecatApp.controller('AboutCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+phonecatApp.controller('ContactCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+
+phonecatApp.controller('PhoneDetailCtrl',['$scope','$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+  $scope.phoneId = $routeParams.phoneId;
+}]);
+
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
     $scope.title = 'Телефоны';
     const obj = {
       "method": "GET",
@@ -12,6 +51,7 @@ phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http
     $http(obj).then(response => {
         $scope.phones = response.data;
     });
+
 }]);
 
 
